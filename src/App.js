b6f2638245b1a47c9b1a0bff59b21a80e38a47d7
anushac14 <113647676+anushac14@users.noreply.github.com>
2023-03-18 15:10:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,81 +1,84 @@
-import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import Papa from "papaparse";
 
-//used this website to parse data https://medium.com/how-to-react/how-to-parse-or-read-csv-files-in-reactjs-81e8ee4870b0
+// Allowed extensions for input file
+const allowedExtensions = ["csv"];
 
-function App() {
-  // State to store parsed data
-  const [parsedData, setParsedData] = useState([]);
+const App = () => {
+	
+	// This state will store the parsed data
+	const [data, setData] = useState([]);
+	
+	// It state will contain the error when
+	// correct file extension is not used
+	const [error, setError] = useState("");
+	
+	// It will store the file uploaded by the user
+	const [file, setFile] = useState("");
 
-  //State to store table Column name
-  const [tableRows, setTableRows] = useState([]);
+	// This function will be called when
+	// the file input changes
+	const handleFileChange = (e) => {
+		setError("");
+		
+		// Check if user has entered the file
+		if (e.target.files.length) {
+			const inputFile = e.target.files[0];
+			
+			// Check the file extensions, if it not
+			// included in the allowed extensions
+			// we show the error
+			const fileExtension = inputFile?.type.split("/")[1];
+			if (!allowedExtensions.includes(fileExtension)) {
+				setError("Please input a csv file");
+				return;
+			}
 
-  //State to store the values
-  const [values, setValues] = useState([]);
+			// If input type is correct set the state
+			setFile(inputFile);
+		}
+	};
+	const handleParse = () => {
+		
+		// If user clicks the parse button without
+		// a file we show a error
+		if (!file) return setError("Enter a valid file");
 
-  const changeHandler = (event) => {
-    // Passing file data (event.target.files[0]) to parse using Papa.parse
-    Papa.parse(event.target.files[0], {
-      header: true,
-      skipEmptyLines: true,
-      complete: function (results) {
-        const rowsArray = [];
-        const valuesArray = [];
+		// Initialize a reader which allows user
+		// to read any file or blob.
+		const reader = new FileReader();
+		
+		// Event listener on reader when the file
+		// loads, we parse it and set the data.
+		reader.onload = async ({ target }) => {
+			const csv = Papa.parse(target.result, { header: true });
+			const parsedData = csv?.data;
+			const columns = Object.keys(parsedData[0]);
+			setData(columns);
+		};
+		reader.readAsText(file);
+	};
 
-        // Iterating data to get column name and their values
-        results.data.map((d) => {
-          rowsArray.push(Object.keys(d));
-          valuesArray.push(Object.values(d));
-        });
-
-        // Parsed Data Response in array format
-        setParsedData(results.data);
-
-        // Filtered Column Names
-        setTableRows(rowsArray[0]);
-
-        // Filtered Values
-        setValues(valuesArray);
-      },
-    });
-  };
-
-  return (
-    <div>
-      {/* File Uploader */}
-      <input
-        type="file"
-        name="file"
-        onChange={changeHandler}
-        accept=".csv"
-        style={{ display: "block", margin: "10px auto" }}
-      />
-      <br />
-      <br />
-      {/* Table */}
-      <table>
-        <thead>
-          <tr>
-            {tableRows.map((rows, index) => {
-              return <th key={index}>{rows}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {values.map((value, index) => {
-            return (
-              <tr key={index}>
-                {value.map((val, i) => {
-                  return <td key={i}>{val}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+	return (
+		<div>
+			<label htmlFor="csvInput" style={{ display: "block" }}>
+				Enter CSV File
+			</label>
+			<input
+				onChange={handleFileChange}
+				id="csvInput"
+				name="file"
+				type="File"
+			/>
+			<div>
+				<button onClick={handleParse}>Parse</button>
+			</div>
+			<div style={{ marginTop: "3rem" }}>
+				{error ? error : data.map((col,
+				idx) => <div key={idx}>{col}</div>)}
+			</div>
+		</div>
+	);
+};
 
 export default App;
