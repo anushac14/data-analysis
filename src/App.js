89d3b6549fdb1c18ae9bcc7e1b585a7a336b3c72
anushac14 <113647676+anushac14@users.noreply.github.com>
2023-03-25 15:33:53 -0400

    the drag and drop seems to work but not sure if data is being parsed

--- src/App.js
+++ src/App.js
@@ -1,84 +1,60 @@
-import React, { useState } from "react";
-import Papa from "papaparse";
+import './App.css';
+import { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import Papa from 'papaparse';
 
-// Allowed extensions for input file
-const allowedExtensions = ["csv"];
+function App() {
+  const [parsedCsvData, setParsedCsvData] = useState([]);
 
-const App = () => {
-	
-	// This state will store the parsed data
-	const [data, setData] = useState([]);
-	
-	// It state will contain the error when
-	// correct file extension is not used
-	const [error, setError] = useState("");
-	
-	// It will store the file uploaded by the user
-	const [file, setFile] = useState("");
+  const parseFile = file => {
+    Papa.parse(file, {
+      header: true,
+      complete: results => {
+        setParsedCsvData(results.data);
+      },
+    });
+  };
 
-	// This function will be called when
-	// the file input changes
-	const handleFileChange = (e) => {
-		setError("");
-		
-		// Check if user has entered the file
-		if (e.target.files.length) {
-			const inputFile = e.target.files[0];
-			
-			// Check the file extensions, if it not
-			// included in the allowed extensions
-			// we show the error
-			const fileExtension = inputFile?.type.split("/")[1];
-			if (!allowedExtensions.includes(fileExtension)) {
-				setError("Please input a csv file");
-				return;
-			}
+  console.log(parsedCsvData);
 
-			// If input type is correct set the state
-			setFile(inputFile);
-		}
-	};
-	const handleParse = () => {
-		
-		// If user clicks the parse button without
-		// a file we show a error
-		if (!file) return setError("Enter a valid file");
+  const onDrop = useCallback(acceptedFiles => {
+    if (acceptedFiles.length) {
+      parseFile(acceptedFiles[0]);
+    }
+  }, []);
 
-		// Initialize a reader which allows user
-		// to read any file or blob.
-		const reader = new FileReader();
-		
-		// Event listener on reader when the file
-		// loads, we parse it and set the data.
-		reader.onload = async ({ target }) => {
-			const csv = Papa.parse(target.result, { header: true });
-			const parsedData = csv?.data;
-			const columns = Object.keys(parsedData[0]);
-			setData(columns);
-		};
-		reader.readAsText(file);
-	};
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: 'text/csv',
+  });
 
-	return (
-		<div>
-			<label htmlFor="csvInput" style={{ display: "block" }}>
-				Enter CSV File
-			</label>
-			<input
-				onChange={handleFileChange}
-				id="csvInput"
-				name="file"
-				type="File"
-			/>
-			<div>
-				<button onClick={handleParse}>Parse</button>
-			</div>
-			<div style={{ marginTop: "3rem" }}>
-				{error ? error : data.map((col,
-				idx) => <div key={idx}>{col}</div>)}
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className="App">
+      <div
+        {...getRootProps({
+          className: `dropzone 
+          ${isDragAccept && 'dropzoneAccept'} 
+          ${isDragReject && 'dropzoneReject'}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag Files Here</p>
+        )}
+      </div>
+
+      <p> {parsedCsvData.data} </p>
+  
+    </div>
+  );
+}
 
 export default App;
