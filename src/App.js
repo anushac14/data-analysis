32ee1235e704c3abfbeaf77567539b03bccc6305
krithikam321 <116663692+krithikam321@users.noreply.github.com>
2023-03-29 21:18:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
+import CsvReader from './CsvReader'
 import './App.css';
-import { useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
-import Papa from 'papaparse';
 
 function App() {
-  const [parsedCsvData, setParsedCsvData] = useState([]);
-
-  const parseFile = file => {
-    Papa.parse(file, {
-      header: true,
-      complete: results => {
-        setParsedCsvData(results.data);
-      },
-    });
-  };
-
-  console.log(parsedCsvData);
-
-  const onDrop = useCallback(acceptedFiles => {
-    if (acceptedFiles.length) {
-      parseFile(acceptedFiles[0]);
-    }
-  }, []);
-
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone({
-    onDrop,
-    accept: 'text/csv',
-  });
-
   return (
     <div className="App">
-      <div
-        {...getRootProps({
-          className: `dropzone 
-          ${isDragAccept && 'dropzoneAccept'} 
-          ${isDragReject && 'dropzoneReject'}`,
-        })}
-      >
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag Files Here</p>
-        )}
-      </div>
-
-  
+      <header className="App-header">
+        <CsvReader />
+      </header>
     </div>
   );
 }
